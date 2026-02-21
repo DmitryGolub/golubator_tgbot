5495f81e054562438e78fbc77b4c3358e7fc3ffe
Arseniy <kazzz-2006@mail.ru>
2026-02-21 17:18:57 +0300

    Add Call model and migration

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -2,4 +2,5 @@ from src.models.user import User
 from src.models.cohort import Cohort
 from src.models.notification import Notification
 from src.models.meeting import Meeting, MeetingUser
+from src.models.call import Call, CallStatus
 from src.models.rule import UserRule, StateRule, CohortRule
