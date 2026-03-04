10f1f3e50d47b989eb8ca017a43e80931afa7c1b
Danil Nasretdinov <nasdneptun26@gmail.com>
2026-03-04 15:53:59 +0300

    + mentor stat

--- src/api/main.py
+++ src/api/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 
+from src.api.routes.mentor import router as mentor_router
 from src.api.routes.survey import router as survey_router
 
 
@@ -10,6 +11,7 @@ def create_app() -> FastAPI:
         description="API для опроса после завершения созвона",
     )
     app.include_router(survey_router)
+    app.include_router(mentor_router)
     return app
 
 
