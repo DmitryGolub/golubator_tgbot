f429d74460b14b8a8650824e0adfbd02864be20c
fazzilka <fazzil349@gmail.com>
2026-03-20 02:12:52 +0300

    fix(feedback): добавить mentor feedback для завершённых встреч

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -2,4 +2,6 @@ from src.models.user import User
 from src.models.cohort import Cohort
 from src.models.notification import Notification
 from src.models.meeting import Meeting, MeetingUser
+from src.models.call import Call
+from src.models.mentor_feedback import MentorFeedback
 from src.models.rule import UserRule, StateRule, CohortRule
